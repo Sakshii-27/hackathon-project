4b3ad1dd016abb21de303419f43ce79560337636
Sakshi Aggarwal <sakshii@Sakshis-MacBook-Pro.local>
2024-11-15 21:29:21 +0530

    second

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# hackathon-project
+A web app for food budgeting.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './/components/navbar/navbar.js'; // Ensure correct path
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />  {/* This renders the Navbar */}
+      {/* Other content */}
     </div>
   );
 }
